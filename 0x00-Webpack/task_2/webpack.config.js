cf2f7ec216862d4265ca645152f25ea3ed4b5aaa
Capnot34 <jenniferamachree2@gmail.com>
2024-04-16 21:29:41 +0000

    added some codes

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,29 +1,34 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production', // Set mode to production
-  entry: './js/dashboard_main.js', // Entry point
-  output: {
-    filename: 'bundle.js', // Output filename
-    path: path.resolve(__dirname, 'public'), // Output directory
+    mode: 'production',
+    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    performance: {
+    maxAssetSize: 1000000,
   },
-  module: {
+    module: {
     rules: [
       {
-        test: /\.css$/, // Rule for processing CSS files
-        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Rule for processing image files
-        type: 'asset/resource', // Use asset modules to handle images
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html', // Specify the HTML template
-    }),
-  ],
-};
-
+        test: /\.(jpg|gif|png|jpeg|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      }
+    ]
+  }
+}
