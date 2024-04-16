391260a48183c654944024761be2aca1f9d5f43a
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2024-04-16 19:16:34 +0100

    Delete task_0 directory

--- task_0/src/index.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$(document).ready(function() {
-  // Add three paragraphs to the body
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
-
