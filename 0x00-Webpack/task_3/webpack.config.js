b017fbf26c99d8aa44256911926cd5912619954a
Capnot34 <jenniferamachree2@gmail.com>
2024-04-17 14:56:03 -0700

    added some codes

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,16 +1,29 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    performance: {
+  mode: "development",
+  entry: path.resolve(__dirname, "js/dashboard_main.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
     maxAssetSize: 1000000,
   },
-    module: {
+  devServer: {
+    port: 8564,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      template: "./index.html",
+    }),
+  ],
+
+  devtool: "inline-source-map",
+
+  module: {
     rules: [
       {
         test: /\.css$/i,
@@ -28,7 +41,8 @@ module.exports = {
             },
           },
         ],
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
+
