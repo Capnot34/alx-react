f52a212b8626253758b7252dd8cdd300d0ae4b10
Capnot34 <jenniferamachree2@gmail.com>
2024-04-18 15:47:44 -0700

    ajusted the notifications page

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,15 +1,31 @@
-// task_1/dashboard/src/Notifications.js
-
-import React from "react";
+import react from "react";
+import { getLatestNotification } from "./utils";
 import "./Notifications.css";
+import closeIcon from "./close-icon.png";
 
 function Notifications() {
+  const handleButtonClick = () => {
+    console.log("Close button has been clicked");
+  };
+
   return (
     <div className="Notifications">
+      <button
+        className="close-button"
+        aria-label="Close"
+        onClick={handleButtonClick}
+        style={{ float: "right" }}
+      >
+        <img src={closeIcon} alt="Close icon" />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
   );
 }
-
 export default Notifications;
 
