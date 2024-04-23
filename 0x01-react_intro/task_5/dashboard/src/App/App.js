fddfcd1f96188c8b9002d791a730c77ac817da7f
jen67 <amakrigift2000@gmail.com>
2024-04-24 00:28:33 +0100

    done

---
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import "../App/App.css";
+import logo from "../assets/holberton-logo.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        <button>OK</button>
+      </div>
+
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)}</p>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(false)}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_5/dashboard/src/utils/utils.test.js
@@ -0,0 +1,28 @@
+import { getFullYear, getFooterCopy } from './utils';
+import { getLatestNotification } from './utils';
+
+describe('getFullYear function', () => {
+  it('returns the current year', () => {
+    const year = getFullYear();
+    expect(typeof year).toBe('number');
+    expect(year).toBe(new Date().getFullYear());
+  });
+});
+
+describe('getFooterCopy function', () => {
+  it('returns "Holberton School" when isIndex is true', () => {
+    expect(getFooterCopy(true)).toBe('Holberton School');
+  });
+
+  it('returns "Holberton School main dashboard" when isIndex is false', () => {
+    expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+  });
+});
+
+describe('getLatestNotification function', () => {
+  it('returns the latest notification string', () => {
+    const notification = getLatestNotification();
+    expect(notification).toBe('<strong>Urgent requirement</strong> - complete by EOD');
+  });
+});
+
