2028c52a30c8192de590525842790b78c82f80fb
jen67 <amakrigift2000@gmail.com>
2024-04-25 16:13:26 +0100

    task 0 done

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import "../App/App.css";
+
+
+function App() {
+  return (
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
